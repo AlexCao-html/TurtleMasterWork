60f45d45de5cdbbe580d1cf8bdd50bfdc18d881d
AlexCao-html <71947510+AlexCao-html@users.noreply.github.com>
2020-10-18 20:41:14 -0700

    Update videos.vue

--- videos.vue
+++ videos.vue
@@ -1,70 +1,14 @@
 <template>
   <h1>Videos</h1>
-   <section class="Videos">
-    <a href="javscript:;" id="Given-video">Click on the one below that you want to learn about</a>
-
-    <p id="Chosen-Video">I want to...</p>
-    <a onclick="
-    video$ = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video$.setAttribute('href', 'https://www.youtube.com/watch?v=n6QwnzbRUyA');
-    video$.setAttribute('target', '_blank');
-    c$video.innerHTML += 'learn about soap vs covid.';
-    ">...learn how to lower your risk of getting covid.</a><br/>
-    <a onclick="
-    video$ = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video$.setAttribute('href', 'https://www.youtube.com/watch?v=FVIGhz3uwuQ');
-    video$.setAttribute('target', '_blank');
-    c$video.innerHTML += 'learn covid vs the flu.';
-    ">...learn covid vs the flu.</a><br/>
-    <a onclick="
-    video$ = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video$.setAttribute('href', 'https://www.youtube.com/watch?v=r6ewlQs8CAQ');
-    video$.setAttribute('target', '_blank');
-    c$video.innerHTML += 'learn what the US did wrong about testing covid.';
-    ">...learn what the US did wrong about testing covid.</a><br/>
-    <a onclick="
-    video = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video.setAttribute('href', 'https://www.youtube.com/watch?v=-LKVUarhtvE');
-    video.setAttribute('target', '_blank');
-    c$video.innerHTML += 'learn about soap vs covid.';
-    ">...learn about soap vs covid.</a><br/>
-<!--      -->
-    <a onclick="
-    video = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video.setAttribute('href', 'https://www.youtube.com/watch?v=O-3Mlj3MQ_Q');
-    video.setAttribute('target', '_blank');
-    c$video.innerHTML += 'learn about covid charts misleading us.';
-    ">...learn about covid charts misleading us.</a><br/>
-    <a onclick="
-    video = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video.setAttribute('href', 'https://www.youtube.com/watch?v=421fmflQX0E');
-    video.setAttribute('target', '_blank');
-    c$video.innerHTML += 'learn about the look of covid.';
-    " href="" target="_blank">...learn about the look of covid.</a><br/>
-    <a onclick="
-    video = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video.setAttribute('href', 'https://www.youtube.com/watch?v=v_kq8_Z4LXQ');
-    video.setAttribute('target', '_blank');
-    c$video.innerHTML += 'learn about soap vs covid.';
-    ">...learn about the symptoms</a><br/>
-    <a onclick="
-    video = document.getElementById('Given-video');
-    c$video = document.getElementById('Chosen-Video');
-    video.removeAttribute('href');
-    video.removeAttribute('target');
-    c$video.innerHTML = '';
-    ">？</a>
-  </section>
+  <a href="https://www.youtube.com/watch?v=n6QwnzbRUyA" target="_blank">click here to learn how to lower your risk of getting covid.</a><br/>
+  <a href="https://www.youtube.com/watch?v=FVIGhz3uwuQ" target="_blank">click here to learn covid vs the flu.</a><br/>
+  <a href="https://www.youtube.com/watch?v=r6ewlQs8CAQ" target="_blank">click here to learn what the US did wrong about testing covid.</a><br/>
+  <a href="https://www.youtube.com/watch?v=-LKVUarhtvE" target="_blank">click here to learn about soap vs covid.</a><br/>
+  <a href="https://www.youtube.com/watch?v=O-3Mlj3MQ_Q" target="_blank">click here to learn about covid charts misleading us.</a><br/>
+  <a href="https://www.youtube.com/watch?v=421fmflQX0E" target="_blank">click here to learn about the look of covid.</a><br/>
+  <a href="https://www.youtube.com/watch?v=U8r3oTVMtQ0" target="_blank">click here to learn about the symptoms</a><br/>
+  <a href="https://www.youtube.com/watch?v=x7KKkElpyKQ" target="_blank">click here to learn about soap vs. hand sanitizer
 </template>
-<script>
-</script>
 <style>
   a{
     display: block;
@@ -78,14 +22,15 @@
     background-color: #000;
     margin: 5px;
     padding: 1vh 1vw;
-    border-radius: 25%;
-    -webkit-border-radius: 25%;
-    -moz-border-radius: 25%;
-    -o-border-radius: 25%;
-    -ms-border-radius: 25%;
+    border-radius: 50%;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    -o-border-radius: 50%;
+    -ms-border-radius: 50%;
+    text-align: center;
+    width: 95vw;
+    height: 5vh;
   }
-
-  a#Given-Video
 
   a:hover{
     color: #bfbfff;
@@ -96,3 +41,5 @@
     -ms-border-radius: 0;
   }
 </style>
+<script>
+</script>
