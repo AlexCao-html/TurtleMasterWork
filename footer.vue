38eee30a23fd9ee5ee33b0f997729c3c290f6a31
AlexCao-html <71947510+AlexCao-html@users.noreply.github.com>
2020-10-18 19:22:08 -0700

    Add files via upload

---
+++ footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer{
+  background-color:#0abf12;
+  border:1px solid #64794d;
+  font-family: cursive;
+  position:fixed;
+  bottom: 0px;
+  width: 100%;
+  left: 0px;
+}
+</style>
---
+++ post.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post">
+    <details>
+      <summary>Comments</summary>
+      <form action="/action_page.php">
+        <textarea name="message" rows="10" cols="30" placeholder="This Site is superb"></textarea>
+
+        <input type="submit">
+      </form>
+    </details>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+details {
+  text-align: center;
+}
+</style>
